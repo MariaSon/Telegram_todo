ff511ef385c78df5c5ad996df298962ddb397245
MariaSon <MashaSog@yandex.ru>
2023-01-02 18:41:20 +0300

    Added verifications

--- telegrambot.py
+++ telegrambot.py
@@ -1,17 +1,17 @@
 import datetime
+import time
 import telebot
 import random
 
-token = open('token.txt', 'r').readline()
-
-bot = telebot.TeleBot(token)
-
 HELP = """
-help - напечатать справку по программе.
-add - добавить задачу в список (название задачи запрашиваем у пользователя).
-show - напечатать все добавленные строки.
-random - добавлять случайную задачу на текущую дату.
+/help - вывод справки по работе бота.
+/add - добавление задачи в список на определенную дату. 
+    Заполняется в формате "/add дд.мм.гггг Задача".
+/show - вывод списка задачи на определенную дату. 
+    Заполняется в формате "/show дд.мм.гггг".
+/random - добавляет случайную задачу на текущую дату.
 """
+
 RANDOM_TASKS = ('Посмотреть новый фильм.',
                 'Прогуляться в парке.',
                 'Сходить в бассейн.',
@@ -23,6 +23,10 @@ RANDOM_TASKS = ('Посмотреть новый фильм.',
                 'Приготовить тортик.',)
 tasks = {}
 person_id = ''
+
+token = open('token.txt', 'r').readline()
+
+bot = telebot.TeleBot(token)
 
 
 def add_todo(date, task):
@@ -38,40 +42,77 @@ def add_todo(date, task):
     bot.send_message(person_id, text)
 
 
+def global_person_id(chat_id):
+    global person_id
+    person_id = chat_id
+
+
+def try_valid_date(date):
+    try:
+        time.strptime(date, '%d.%m.%Y')
+        today = datetime.datetime.now().strftime('%d.%m.%Y')
+        if date < today:
+            return bot.send_message(person_id,
+                                    f'Ошибка! Указанная Вами дата "{date}" меньше текущей "{today}"!')
+    except ValueError:
+        return bot.send_message(person_id,
+                                f'Дата {date} не существует, либо указан некорректный формат'), False
+
+
 @bot.message_handler(commands=['help'])
 def help_tg(message):
-    bot.send_message(message.chat.id, HELP)
+    global_person_id(message.chat.id)
+    bot.send_message(person_id, HELP)
 
 
 @bot.message_handler(commands=['add', 'random'])
 def add_random_tg(message):
-    global person_id
-    person_id = message.chat.id
+    global_person_id(message.chat.id)
     if '/add' in message.text:
         split_com = message.text.split(maxsplit=2)
+        try:
+            split_com[1]
+        except IndexError:
+            return bot.send_message(person_id,
+                                    f'Ошибка! Указан неверный формат для добавления задачи в список!'
+                                    f'Заполняется в формате "/add дд.мм.гггг Задача".')
         date = split_com[1]
+        if try_valid_date(date):
+            return bot.send_message(person_id,
+                                    f'Попробуйте еще раз!')
+
         task = split_com[2]
     elif message.text == '/random':
         task = random.choice(RANDOM_TASKS)
-        date = datetime.datetime.now().strftime('%d/%m/%Y').replace('/', '.')
+        date = datetime.datetime.now().strftime('%d.%m.%Y')
     add_todo(date, task)
 
 
 @bot.message_handler(commands=['show'])
 def show_tg(message):
+    global_person_id(message.chat.id)
     try:
-        date = message.text.split(maxsplit=1)[1]
+        message.text.split(maxsplit=1)[1]
     except IndexError:
-        return bot.send_message(message.chat.id, 'Для просмотра задач необходимо ввести '
-                                                 'команду в формате "/show дд.мм.гггг"')
+        return bot.send_message(person_id,
+                                f'Ошибка! Указан неверный формат для вывода списка задач!'
+                                f'Заполняется в формате "/show дд.мм.гггг".')
+    date = message.text.split(maxsplit=1)[1]
+    if try_valid_date(date):
+        return bot.send_message(person_id,
+                                f'Попробуйте еще раз!')
     if date in tasks:
         text = f'Задачи для выполнения по сроку {date}:' + '\n'
         for task in tasks[date]:
             text = text + ' - ' + task + '\n'
     else:
         text = f'Задачи на дату {date} отсутстуют. Можете отдохнуть!'
-    bot.send_message(message.chat.id, text)
+    bot.send_message(person_id, text)
+
+
+@bot.message_handler(content_types=['text'])
+def incorrect_command(message):
+    help_tg(message)
 
 
 bot.polling(none_stop=True)
-
